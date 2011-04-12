fd75280a0304a8717b60874336c2d64326ea0367
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-04-12 12:17:09 +0700

    SAND-81: add lost files

--- lib/Gallery/Schema/Result/Comment.pm
+++ lib/Gallery/Schema/Result/Comment.pm
@@ -1,14 +1,68 @@
-package Gallery::Schema::Result::Comments;
-use base "DBIx::Class";
-__PACKAGE__->load_components(qw/TimeStamp Core/);
+package Gallery::Schema::Result::Comment;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 NAME
+
+Gallery::Schema::Result::Comment
+
+=cut
+
 __PACKAGE__->table("comments");
+
+=head1 ACCESSORS
+
+=head2 comment_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 comment
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 c_date
+
+  data_type: 'datetime'
+  default_value: CURRENT_DATE
+  is_nullable: 1
+
+=cut
+
 __PACKAGE__->add_columns(
-    "comment_id"	=> { data_type => "INT", 		is_nullable => 0, },
-	"comment" 		=> { data_type => "VARCHAR", 	is_nullable => 1, size => 255, },
-	"c_date" 		=> { data_type => 'datetime',	set_on_create => 1, set_on_update => 1 }	
+  "comment_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "comment",
+  { data_type => "varchar", is_nullable => 1 },
+  "c_date",
+  {
+    data_type     => "datetime",
+    default_value => \"CURRENT_DATE",
+    is_nullable   => 1,
+  },
 );
-__PACKAGE__-> set_primary_key( "comment_id" );
+__PACKAGE__->set_primary_key("comment_id");
+#set relationships
 __PACKAGE__-> has_many( 'user_comments' => 'Gallery::Schema::Result::User_comments', { "foreign.comment_id" => "self.comment_id" });
 __PACKAGE__-> many_to_many( uses => 'user_comments', 'user');
 
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-04-12 11:28:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:qQUpVx0hBdcdoSztOcI0dg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
 1;
